73c9cb772f58162496d93186b8662187b14e0279
Georg Pohl <georgpohl@gmx.de>
2025-07-15 05:29:44 +0200

    Add second approach to read from a file.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,15 @@
 use std::fs::File;
 use std::io::{self, BufRead, BufReader};
+use clap::Parser;
+use anyhow::{Context, Result};
 
+#[derive(Parser)]
+struct Cli {
+    pattern: String,
+    path: std::path::PathBuf, // PathBuf works cross-platform
+}
+
+// This program reads a file line by line and prints each character
 fn main() -> io::Result<()> {
     let file = File::open("src/main.rs")?;
     let reader = BufReader::new(file);
@@ -15,6 +24,22 @@ fn main() -> io::Result<()> {
 	    print!("{}", char);
 	}
 	println!(); // Print a newline after each line
+    }
+    Ok(())
+}
+
+// This program reads the whole file into a string
+fn main() -> Result<()> {
+    let args = Cli::parse();
+
+    // let content = std::fs::read_to_string(&args.path).expect("Failed to read file"); // Readsfile into a string
+    let content = std::fs::read_to_string(&args.path)
+	.with_context(|| format!("could not read file: '{}'", args.path.display()))?;
+    
+    for line in content.lines() {
+	if line.contains(&args.pattern) {
+	    println!("{}", line);
+	}
     }
     Ok(())
 }
